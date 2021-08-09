4d31fa2d9f11475c71a726a1508ebfdbe40839ff
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-09 12:39:46 -0300

    Update 1012 - Área.c

--- 1012 - Área.c
+++ 1012 - Área.c
@@ -2,10 +2,10 @@
 #include <math.h>
 int main() {
  
-    double a, b, c;
-    scanf("%lf%lf%lf", &a, &b, &c);
+    double a, b, c;                                 /*variaveis necessarias*/
+    scanf("%lf%lf%lf", &a, &b, &c);                 /*recebe os valores*/
     
-    printf("TRIANGULO: %.3lf\n", a*c/2.0);
+    printf("TRIANGULO: %.3lf\n", a*c/2.0);          /*calcula e imprime*/
     printf("CIRCULO: %.3lf\n", 3.14159*pow(c, 2));
     printf("TRAPEZIO: %.3lf\n", (a+b)*c/2.0);
     printf("QUADRADO: %.3lf\n", pow(b, 2));
