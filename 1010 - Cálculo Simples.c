7b93a527e1997640cf3fc78110f79b451a1bdd61
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-09 12:33:20 -0300

    Update 1010 - Cálculo Simples.c

--- 1010 - Cálculo Simples.c
+++ 1010 - Cálculo Simples.c
@@ -2,14 +2,14 @@
  
 int main() {
  
-    int cod[2], numero[2], i;
-    float valor[2], total = 0;
+    int cod[2], numero[2], i;                              /*variaveis do cod e quantidade, e um array[2] pq serao duas entradas*/
+    float valor[2], total = 0;                             /*variaveis do valor e total*/
     
     for(i = 0; i<2; ++i){
-        scanf("%d%d%f", &cod[i], &numero[i], &valor[i]);
-        total = total + valor[i] * numero[i];
+        scanf("%d%d%f", &cod[i], &numero[i], &valor[i]);   /*recebe os dados do usario*/
+        total = total + valor[i] * numero[i];              /*calcula o valor*/
     }
-    printf("VALOR A PAGAR: R$ %.2f\n", total);
+    printf("VALOR A PAGAR: R$ %.2f\n", total);             /*imprime*/
     
  
     return 0;
