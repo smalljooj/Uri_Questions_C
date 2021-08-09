30c1d1ed054e9e6621fe0e8871b8a49095b43c08
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-09 12:20:07 -0300

    Update 1009 - Salário com Bônus.c

--- 1009 - Salário com Bônus.c
+++ 1009 - Salário com Bônus.c
@@ -2,12 +2,12 @@
  
 int main() {
  
-    char nome[61]; /*variavel para o nome*/
-    double salario, vendas; /* variaveis para salario e vendas*/
+    char nome[61];                                /*variavel para o nome*/
+    double salario, vendas;                       /* variaveis para salario e vendas*/
     
-    scanf("%s%lf%lf", nome, &salario, &vendas); /*atribui valor as variaveis*/
-    salario = salario + vendas*0.15; /* calcula o salario*/
-    printf("TOTAL = R$ %.2lf\n", salario); /**/
+    scanf("%s%lf%lf", nome, &salario, &vendas);   /*atribui valor as variaveis*/
+    salario = salario + vendas*0.15;              /* calcula o salario*/
+    printf("TOTAL = R$ %.2lf\n", salario);        /*imprime*/
  
     return 0;
 }
