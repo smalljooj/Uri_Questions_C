a2638a3c0348557c3e2c9e83e3b5cf5d753bb091
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-09 12:37:37 -0300

    Update 1011 - Esfera.c

--- 1011 - Esfera.c
+++ 1011 - Esfera.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h>                                 /*Nesse adicionamos a biblioteca math, para usarmos operacoes matematicas como exponenciacao e raiz quadrada*/
 
 int main() {
  
-    double r, v;
-    const double pi = 3.14159;
+    double r, v, pi = 3.14159;                    /*variaveis para o raio e volume e pi*/
     
-    scanf("%lf", &r);
+    scanf("%lf", &r);                             /*recebe o valor do raio*/
     
-    v = (4 * pi * pow(r, 3))/3;
-    printf("VOLUME = %.3lf\n", v);
+    v = (4 * pi * pow(r, 3))/3;                   /*calcula o volume, a funcao pow serve para fazer exponenciacao*/
+    printf("VOLUME = %.3lf\n", v);                /*imprime*/
     
  
     return 0;
