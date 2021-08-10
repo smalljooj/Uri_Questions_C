804ffdbc1bb19552e24f53b003bff8d882cef5be
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-09 23:20:54 -0300

    Create  1045 - Tipos de Triângulos.c

---
+++ 1045 - Tipos de Triângulos.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/*
+  Essa fiz questao de fazer com ifs, pra dar uma exercitada leve.
+*/
+
+
+int main() {
+ 
+    float a, b, c, d;             /*variaveis necessarias, a b c para lados, d para auxiliar na troca*/
+    scanf("%f%f%f", &a,&b,&c);    /*ler e atribui valor as variaveis*/
+
+    if(a<b){                       /*deixa os valores em ordem decrescente*/
+        d = a;
+        a = b;
+        b = d;
+        if(b<c){
+            d = b;
+            b = c;
+            c = d;
+        }
+        if(a<b){
+            d = a;
+            a = b;
+            b = d;
+        }
+    }else if(a<c){
+        d = a;
+        a = c;
+        c = d;
+        d = c;
+        c = b;
+        b = d;
+    }else if(b<c){
+        d = c;
+        c = b;
+        b = d;
+    }
+    
+    if(a>=(b+c))                                /*verifica se existe triangulo*/
+        printf("NAO FORMA TRIANGULO\n");
+    else{                                       /*senao verifica que tipos de triangulo ele é*/
+        if((a*a)==((b*b)+(c*c)))
+            printf("TRIANGULO RETANGULO\n");
+        if((a*a)>((b*b)+(c*c)))
+            printf("TRIANGULO OBTUSANGULO\n");
+        if((a*a)<((b*b)+(c*c)))
+            printf("TRIANGULO ACUTANGULO\n");
+        if(a==b && b==c)
+            printf("TRIANGULO EQUILATERO\n");
+        else if((a==b || b==c || c==a))
+            printf("TRIANGULO ISOSCELES\n");
+    }
+    return 0;
+}
