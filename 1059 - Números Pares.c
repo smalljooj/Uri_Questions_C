18414c7c2e5cab84a0f93757a3224fb2179e9d8a
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-10 15:20:13 -0300

    Create 1059 - Números Pares.c

---
+++ 1059 - Números Pares.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Fiz apenas um loop de 2 a 100, incluindo o 100 e imprimi*/
+
+int main() {
+    int i;
+    for(i=2; i<=100; i+=2){
+        printf("%d\n", i);
+    }
+ 
+    return 0;
+}
