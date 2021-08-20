dd44281f6d7befb442337652578fe2b52af1e74f
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-20 16:40:17 -0300

    Create 1186 - Abaixo da Diagonal Secundária.c

---
+++ 1186 - Abaixo da Diagonal Secundária.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void limpabuf(){
+    char a;
+    while((a = getchar()!='\n'));
+}
+
+int main() {
+ 
+    int j, i, cont = 0;
+    char c;
+    float a[12][12], soma = 0;
+
+    scanf("%c", &c);
+
+    for(i = 0; i<12; i++)
+        for(j = 0; j<12; j++)
+            scanf("%f", &a[i][j]);
+            
+    for(i = 0; i<12; i++)
+        for(j = 12-i; j<12; j++){
+              soma+=a[i][j]; 
+              cont++;
+        }
+        
+    if(c=='S')
+        printf("%.1f\n", soma);
+    else 
+        printf("%.1f\n", soma/cont);
+    return 0;
+}
