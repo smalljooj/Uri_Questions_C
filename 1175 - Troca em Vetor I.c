da4722481d2397b0cc24ef97736ae3f0e3ed1e79
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-20 11:39:00 -0300

    Create 1175 - Troca em Vetor I.c

---
+++ 1175 - Troca em Vetor I.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a[20], x, i, j;
+    for(i = 0; i<20; i++)
+        scanf("%d", &a[i]);
+    for(i =0, j = 19; j>=0;j--, i++){
+        printf("N[%d] = %d\n", i, a[j]);
+    }
+    return 0;
+}
