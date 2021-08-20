bc2bfadf3234f867475b9f251777e31a85aeceb5
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-20 19:41:21 -0300

    Create 1189 - Área Esquerda.c

---
+++ 1189 - Área Esquerda.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void limpabuf(){
+    char a;
+    while((a = getchar()!='\n'));
+}
+
+int main() {
+
+    int j, i, cont = 0;
+    char c;
+    double a[12][12], soma = 0;
+
+    scanf("%c", &c);
+
+    for(i = 0; i<12; i++)
+        for(j = 0; j<12; j++)
+            scanf("%lf", &a[i][j]);
+
+    for(i = 1; i<6; i++)
+        for(j = 0; j<i; j++){
+            soma+=a[i][j]; 
+            cont++;
+        }
+    for(; i<11; i++)
+        for(j = 0; j<11-i; j++){
+            soma+=a[i][j]; 
+            cont++;
+        }
+
+    if(c=='S')
+        printf("%.1lf\n", soma);
+    else 
+        printf("%.1lf\n", soma/cont);
+    return 0;
+}
