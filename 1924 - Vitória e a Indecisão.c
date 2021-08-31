c7d14f35e87290abbbb32ad732c9727c963efa31
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-08-31 12:57:34 -0300

    Create 1924 - Vitória e a Indecisão.c

---
+++ 1924 - Vitória e a Indecisão.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int i, x;
+    char a[100];
+    scanf("%d", &x);
+    
+    for(i = 0; i<x; i++)
+        scanf("%s", a);
+    printf("Ciencia da Computacao\n");
+ 
+    return 0;
+}
