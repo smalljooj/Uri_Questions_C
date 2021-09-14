931115587d5e17b1b45ba5282f7291a53e32a933
Ismael Marinho <51092781+smalljooj@users.noreply.github.com>
2021-09-14 15:13:30 -0300

    Create 2160 - Nome no Formulário.c

---
+++ 2160 - Nome no Formulário.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    char a[500];
+    int i, cont = 0;
+    
+    scanf("%[^\n]s" , a);
+    
+    for(i = 0; a[i]!='\0'; i++){
+        cont++;
+    }
+    if(cont<=80)
+        printf("YES\n");
+    else
+        printf("NO\n");
+        
+    return 0;
+}
